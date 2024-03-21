a6ad7928f0aec6f22ee085905f712b0f1d5a6a36
kunal <kunalrisaanva12@gmail.com>
2024-03-21 11:52:09 +0530

    update mogno url

--- src/dbConnection/dbConnection.js
+++ src/dbConnection/dbConnection.js
@@ -2,10 +2,10 @@ import { connect } from "mongoose";
 import { DB_Name } from "../constant.js";
 
 
-const DbConnection = async() => {
+const DbConnection = async () => {
     try {
-        const connectionInstance = await connect(`mongodb://localhost:27017/${DB_Name}`);
-        console.log(`\n Mongodb Connected !! DB Host: ${connectionInstance.connection.host}`)
+      const connectionInstence =  await connect(`${process.env.MONGOURI}/${DB_Name}`);
+      console.log(`\n MongoDB Connected !! DB HOST : ${connectionInstence.connection.host}`);
     } catch (error) {
         console.log('Mongodb connection Failed ',error);
         process.exit(1)
